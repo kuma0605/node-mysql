37ee07b82ae78365c256792096094c229d69dbe4
dylan <gbujieshi@gmail.com>
2017-08-17 18:59:43 +0800

    amend query

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     // port: 3306 //default to 3306 if omitted.
 });
 
-connection.query(
+/*connection.query(
     // 'SELECT "foo" AS first_field, "bar" AS second_field',
     'SELECT id, content FROM test',
     function(err, results, fields){
@@ -22,4 +22,28 @@ connection.query(
         }
         connection.end();
     }
-);
+);*/
+
+var query = connection.query('SELECT id, content FROM test');
+
+/*query.on('row', function(row){
+    console.log('Content of id '+ row.id+ ' is '+ row.content);
+})*/
+
+/*query.on('error',function(err){
+    console.log('A database error occured:')
+    console.log(err)
+});
+
+query.on('fields', function(fields){
+    console.log(fields)
+});*/
+
+query.on('result',function(result){
+    console.log('Received result:', result);
+})
+
+query.on('end',function(){
+    console.log('Query execution has finished.');
+    connection.end();
+})
