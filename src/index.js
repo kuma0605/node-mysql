a7d1140990f6177e0bf95d53ac1ea789c7e439a1
dylan <gbujieshi@gmail.com>
2017-08-17 16:59:47 +0800

    add database node

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"rourou"
+    password:"rourou",
+    database:'node'
     // port: 3306 //default to 3306 if omitted.
 });
 
 connection.query(
-    'SELECT "foo" AS first_field, "bar" AS second_field',
+    // 'SELECT "foo" AS first_field, "bar" AS second_field',
+    'SELECT id, content FROM test',
     function(err, results, fields){
-        console.log(err)
-        console.log(results);
+        // console.log(fields);
+        if(err){
+            console.log("A database error occured!");
+        }else{
+            console.log(results)
+        }
         connection.end();
     }
-
-    )
+);
