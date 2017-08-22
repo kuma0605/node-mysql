38779c5c668b395c56eb58bc0dbb965a25b79744
dylan <gbujieshi@gmail.com>
2017-08-22 19:22:49 +0800

    complete database

--- src/index_b.js
+++ src/index_b.js
@@ -2,7 +2,7 @@
 
 var mysql = require("mysql"),
     http = require('http'),
-    url = require('url');
+    url = require('url'),
     querystring = require('querystring');
 
 
@@ -37,22 +37,24 @@ function handleRequest(request, response){
                         </form>
                         </body>
                         </html>`;
-
+    //Parsing the requested URL path in order to distinguish between 
+    //the / page and the /add route
     var pathname = url.parse(request.url).pathname;
 
     //User wants to add content to the database (POST request to /add)
     if(pathname == '/add'){
         var requestBody = '';
-        var postParameters = '';
+        var postParameters;
         request.on('data',function(data){
             requestBody+=data;
         });
         request.on('end',function(){
             postParameters = querystring.parse(requestBody);
+            //The content to be added is in POST parameter "content"
             addContentToDatabase(postParameters.content, function(){
                 //Redirect back to homepage when the database has finished adding
                 //the new content to the database
-                response.writeHead(302, {'Locatoin':'/'});
+                response.writeHead(302, {'Location':'/'});
                 response.end();
             })
         })
@@ -71,6 +73,10 @@ function handleRequest(request, response){
     }
 }
 
+//Function that is called by the code that handles the / route and 
+//retrieves contents from the database, applying a LIKE filter if one
+//was supplied
+
 function getContentsFromDatabase(filter, callback){
     var connection = mysql.createConnection({
         host:'localhost',
@@ -83,6 +89,49 @@ function getContentsFromDatabase(filter, callback){
     var resultAsString = '';
 
     if(filter){
-        query = connection.query('SELECT id, content FROM ')
+        query = connection.query('SELECT id, content FROM test' +
+                                 'WHERE content LIKE "'+ filter +'%"');
+    }else{
+        query = connection.query('SELECT id, content FROM test');
     }
+
+    query.on('error',function(err){
+        console.log('A database error occured:');
+        console.log(err);
+    });
+
+    //With every result, build the string that is later replaced into
+    //the HTML of the homepage
+    query.on('result', function(result){
+        resultAsString+='id:'+result.id;
+        resultAsString+=', content:'+result.content;
+        resultAsString+='\n';
+    });
+
+    query.on('end',function(result){
+        connection.end();
+        callback(resultAsString);
+    });
+}
+
+//Function that is called by the code that handles the /add route
+//and inserts the supplied string as a new content entry
+
+function addContentToDatabase(content, callback){
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'rourou',
+        database:'node'
+    });
+
+    connection.query('INSERT INTO test (content)'+
+                    'VALUES ("'+ content +'")',
+                    function(err){
+                        if(err){
+                            console.log('Could not insert content "' +content+
+                                '" into database.');
+                        }
+                        callback();
+                    })
 }
